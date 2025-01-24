db960e23ad1f79553dba97f38464b5984dbb8045
Kenneth Fechter <kenneth.fechter@gmail.com>
2025-01-23 21:26:50 -0500

    Update project links and photos

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -9,39 +9,47 @@ const featuredProjects = [
         id: 1,
         title: "BudgetKeeper",
         description: ".NET Core Razor Pages app for tracking credit cards and loans",
-        image: "/images/hero-image.png",
-        gitUrl: "/",
-        previewUrl: "/",
+        image: "/images/projects/budgetkeeper.png",
+        gitUrl: "https://github.com/kfechter/BudgetKeeper",
+        previewUrl: "https://github.com/kfechter/BudgetKeeper",
         tag: ["All", "Web"]
     },
     {
         id:2,
         title: "Calculendar",
         description: "Android App for calculating number of days between dates",
-        image: "/images/hero-image.png",
-        gitUrl: "/",
-        previewUrl: "/",
+        image: "/images/projects/calculendar.png",
+        gitUrl: "https://github.com/kfechter/Calculendar",
+        previewUrl: "https://play.google.com/store/apps/details?id=com.kennethfechter.calculendar",
         tag: ["All","Mobile"]
     },
     {
         id:3,
         title: "Lab551",
-        description: "Android NFC App for card access",
-        image: "/images/hero-image.png",
-        gitUrl: "/",
-        previewUrl: "/",
+        description: "Formerly an HCE access app, will be rebuilt as a new app",
+        image: "/images/projects/lab-551.png",
+        gitUrl: "https://github.com/kfechter/Lab551",
+        previewUrl: "https://github.com/kfechter/Lab551",
         tag: ["All","Mobile"]
     },
     {
         id: 4,
         title: "LaTeX Resume",
         description: "LaTeX resume with CI/CD Pipeline for publishing",
-        image: "/images/hero-image.png",
-        gitUrl: "/",
-        previewUrl: "/",
+        image: "/images/projects/resume.png",
+        gitUrl: "https://github.com/kfechter/kfechter-resume-latex",
+        previewUrl: "https://github.com/kfechter/kfechter-resume-latex",
         tag: ["All", "Misc"]
     },
-
+    {
+        id: 5,
+        title: "VSCode Extension",
+        description: "Helper for MU Tianocore dependency updates",
+        image: "/images/projects/extension.png",
+        gitUrl: "https://github.com/kfechter/mu-tiano-dependency-updater",
+        previewUrl: "https://marketplace.visualstudio.com/items?itemName=thesecondken.mu-tiano-dependency-updater",
+        tag: ["All", "Misc"]
+    },
 ]
 
 export default function ProjectsSection() {
