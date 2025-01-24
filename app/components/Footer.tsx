b637be9bd84a7aef5983ebd57dd8359b43de7a3f
Kenneth Fechter <kenneth.fechter@gmail.com>
2025-01-23 21:29:46 -0500

    Update LOGO to KFechter

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
         <div className="container p-12 flex justify-between flex-row">
-            <span className="mr-150">LOGO</span>
+            <span className="mr-150">KFechter</span>
             <p className="text-slate-400">&copy; {(new Date().getFullYear())}, All Rights Reserved.</p>
         </div>
     </footer>
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -22,7 +22,7 @@ export default function NavBar() {
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
         <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
-            <Link href="/" className="text-xl md:text-2xl text-white font-semibold">LOGO</Link>
+            <Link href="/" className="text-xl md:text-2xl text-white font-semibold">KFechter</Link>
             <div className="mobile-menu block md:hidden">
                 {
                     !navbarOpen ? (
